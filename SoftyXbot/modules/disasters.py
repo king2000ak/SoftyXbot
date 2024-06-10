dddcaaaeb9720aeb87fdb05602a971112e57a800
⫷ Aɳαɳԃ Sԋαɾɱα ⫸ <91930171+BadshahAk@users.noreply.github.com>
2024-06-10 12:51:34 +0530

    Update disasters.py

--- SoftyXbot/modules/disasters.py
+++ SoftyXbot/modules/disasters.py
@@ -561,7 +561,7 @@ dispatcher.add_handler(SUPPORTLIST_HANDLER)
 dispatcher.add_handler(SUDOLIST_HANDLER)
 dispatcher.add_handler(DEVLIST_HANDLER)
 
-__mod_name__ = "Dᴇᴠꜱ"
+__mod_name__ = "🦚Dᴇᴠꜱ🐣"
 __handlers__ = [
     SUDO_HANDLER,
     SUPPORT_HANDLER,
