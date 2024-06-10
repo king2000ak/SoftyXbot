f4e0ed8f27150868b5364b9fc45472b3ea9af350
⫷ Aɳαɳԃ Sԋαɾɱα ⫸ <91930171+BadshahAk@users.noreply.github.com>
2024-06-10 12:46:01 +0530

    Update cust_filters.py

--- SoftyXbot/modules/cust_filters.py
+++ SoftyXbot/modules/cust_filters.py
@@ -625,7 +625,7 @@ __help__ = """
 
 """
 
-__mod_name__ = "Fɪʟᴛᴇʀs"
+__mod_name__ = "🦚Fɪʟᴛᴇʀs🐣"
 
 FILTER_HANDLER = CommandHandler("filter", filters, run_async=True)
 STOP_HANDLER = CommandHandler("stop", stop_filter, run_async=True)
