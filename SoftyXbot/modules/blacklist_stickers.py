d5245e9b65d64dab325dfb02afb6603e5f51564e
⫷ Aɳαɳԃ Sԋαɾɱα ⫸ <91930171+BadshahAk@users.noreply.github.com>
2024-06-10 12:34:21 +0530

    Update blacklist_stickers.py

--- SoftyXbot/modules/blacklist_stickers.py
+++ SoftyXbot/modules/blacklist_stickers.py
@@ -501,7 +501,7 @@ __help__ = """
  ❍ <sticker link > ᴄᴀɴ ʙᴇ `https://t.me/addstickers//<sticker>` ᴏʀ ᴊᴜsᴛ `<sticker>` ᴏʀ ʀᴇᴘʟʏ ᴛᴏ ᴛʜᴇ sᴛɪᴄᴋᴇʀ ᴍᴇssᴀɢᴇ
 """
 
-__mod_name__ = "Sᴛɪᴄᴋᴇʀ"
+__mod_name__ = "🦚Sᴛɪᴄᴋᴇʀ🐣"
 
 BLACKLIST_STICKER_HANDLER = DisableAbleCommandHandler(
     "blsticker", blackliststicker, admin_ok=True, run_async=True
