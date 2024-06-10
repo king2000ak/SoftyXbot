bd7ac1a2aec9f0108814e82a329e0bc9de50b10a
⫷ Aɳαɳԃ Sԋαɾɱα ⫸ <91930171+BadshahAk@users.noreply.github.com>
2024-06-10 12:46:45 +0530

    Update dbcleanup.py

--- SoftyXbot/modules/dbcleanup.py
+++ SoftyXbot/modules/dbcleanup.py
@@ -140,5 +140,5 @@ BUTTON_HANDLER = CallbackQueryHandler(callback_button, pattern="db_.*", run_asyn
 dispatcher.add_handler(DB_CLEANUP_HANDLER)
 dispatcher.add_handler(BUTTON_HANDLER)
 
-__mod_name__ ="Dᴀᴛᴀʙᴀsᴇ"
+__mod_name__ ="🦚Dᴀᴛᴀʙᴀsᴇ🐣"
 __handlers__ = [DB_CLEANUP_HANDLER, BUTTON_HANDLER]
